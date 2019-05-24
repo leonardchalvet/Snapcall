d24cf068d2fb3aacb3ace5fcc9b4ce7a1b622876
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-05-24 16:33:32 +0200

    Page about - Page 404 - illu product

---
+++ 404.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+
+		<title>404</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/404.css">
+
+		<script type="text/javascript" src="script/minify/jQuery.3.3.1-min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section id="section-content">
+				<img class="obj obj-1" src="img/404/obj-1.svg">
+				<img class="obj obj-2" src="img/404/obj-2.svg">
+				<img class="obj obj-3" src="img/404/obj-3.svg">
+				<div class="wrapper">
+					<div class="container-text">
+						<h1>
+							404<br><span>error.</span>
+						</h1>
+						<p>
+							Page not found. But it could be worse. For example a bad call...
+						</p>
+						<div class="container-button">
+							<a href="" class="cm-button-style1">
+								<span class="btn-text">Get Started</span>
+								<span class="container-btn-arrow">
+									<svg class="btn-arrow" viewBox="0 0 23 22">
+									   <use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/common-min.js"></script>
+	</body>
+</html>
---
+++ about.php
@@ -0,0 +1,320 @@
+<html>
+	<head>
+
+		<title>About</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/about.css">
+
+		<script type="text/javascript" src="script/minify/jQuery.3.3.1-min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section id="section-cover">
+				<div class="wrapper">
+					<div class="container-background">
+						<div class="background"></div>
+					</div>
+					<div class="container-text">
+						<h1>
+							About
+							<br><span>Us.</span>
+						</h1>
+						<p>
+							We believe in the power of voice, warm conversations and snap connections
+						</p>
+						<div class="container-button">
+							<a href="" class="cm-button-style1">
+								<span class="btn-text">Get Started</span>
+								<span class="container-btn-arrow">
+									<svg class="btn-arrow" viewBox="0 0 23 22">
+									   <use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+
+					</div>
+					<div class="container-illu">
+						<div class="illu" style="background-image: url(img/about/cover-photo.jpg);">
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-pr" class="sectionAnim_container">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="el">
+							<div class="line elAnim__slide anim__delayMedium_0"></div>
+							<h4 class="elAnim__slide anim__delayMedium_1">
+								600K
+							</h4>
+							<p class="elAnim__slide anim__delayMedium_2">
+								Calls and counting	
+							</p>
+						</div>
+
+						<div class="el">
+							<div class="line elAnim__slide anim__delayMedium_1"></div>
+							<h4 class="elAnim__slide anim__delayMedium_2">
+								1M€
+							</h4>
+							<p class="elAnim__slide anim__delayMedium_3">
+								In venture funding
+							</p>
+						</div>
+
+						<div class="el">
+							<div class="line elAnim__slide anim__delayMedium_2"></div>
+							<h4 class="elAnim__slide anim__delayMedium_3">
+								10
+							</h4>
+							<p class="elAnim__slide anim__delayMedium_4">
+								People in our team
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-story">
+				<div class="wrapper">
+					<div class="container-photo">
+						<div class="bg" style="background-image: url(img/about/story-photo.jpg);"></div>
+					</div>
+					<div class="container-text">
+						<h2>
+							Our story.
+						</h2>
+						<p>
+							After working several years in the voice industry we realized everything seemed to change, become digital and smarter but phone systems. The foundation of phone systems haven’t changed much in the last 128 years and we decided to do something about it. That’s how SnapCall was born, we built our product around the end-customer, to make calling a business a delightful experience and not tedious, for once! And for companies we decided to tell them, you don’t need your old phone numbers on your website or app, one smart button will make the job. As simple and powerful as that.
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-numbers">
+				<div class="wrapper">
+					<div class="container-text sectionAnim_container">
+						<h2 class="elAnim__slide anim__delayMedium_0">
+							The road we’ve taken.
+						</h2>
+						<p class="elAnim__slide anim__delayMedium_1">
+							We are bold and decided to build the best voice product out there. No shortcuts or third-party providers. Our voice technology is built all in-house.
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el sectionAnim_container">
+							<div class="container-photo elAnim__fade anim__delayMedium_3">
+								<div class="bg" style="background-image: url(img/about/section-numbers/photo-1.jpg)"></div>
+							</div>
+							<div class="text">
+								<div class="row">
+									<div class="number elAnim__slide anim__delayMedium_0">
+										1.
+									</div>
+									<h3 class="elAnim__slide anim__delayMedium_1">
+										Taking the leap. 
+									</h3>
+								</div>
+								<p class="elAnim__slide anim__delayMedium_2">
+									We left our corporate jobs and founded SnapCall in 2016. We worked at voice and internet providers companies for a long time and realized voice remained on a different and disconnected silo to the digital world. Companies were missing valuable data and pushing customers out of their digital journeys when using voice.
+								</p>
+							</div>
+						</div>
+						<div class="el sectionAnim_container">
+							<div class="container-photo elAnim__fade anim__delayMedium_3">
+								<div class="bg" style="background-image: url(img/about/section-numbers/photo-2.jpg)"></div>
+							</div>
+							<div class="text">
+								<div class="row">
+									<div class="number elAnim__slide anim__delayMedium_0">
+										2.
+									</div>
+									<h3 class="elAnim__slide anim__delayMedium_1">
+										Joining the experts. 
+									</h3>
+								</div>
+								<p class="elAnim__slide anim__delayMedium_2">
+									In 2017 we joined Zendesk incubation program at Station F in Paris. We joined the worldwide experts on customer experience to enrich our product and help companies to add voice on a smarter way, a way that has never been done before.
+								</p>
+							</div>
+						</div>
+						<div class="el sectionAnim_container">
+							<div class="container-photo elAnim__fade anim__delayMedium_3">
+								<div class="bg" style="background-image: url(img/about/section-numbers/photo-3.jpg)"></div>
+							</div>
+							<div class="text">
+								<div class="row">
+									<div class="number elAnim__slide anim__delayMedium_0">
+										3.
+									</div>
+									<h3 class="elAnim__slide anim__delayMedium_1">
+										Leveraging funds for our mission. 
+									</h3>
+								</div>
+								<p class="elAnim__slide anim__delayMedium_2">
+									In 2018 we raised 1 million euros from leading French VC, ISAI. We are backed by a strong partner in our mission to disrupt voice and how companies communicate with customers.
+								</p>
+							</div>
+						</div>
+						<div class="el sectionAnim_container">
+							<div class="container-photo elAnim__fade anim__delayMedium_3">
+								<div class="bg" style="background-image: url(img/about/section-numbers/photo-4.jpg)"></div>
+							</div>
+							<div class="text">
+								<div class="row">
+									<div class="number elAnim__slide anim__delayMedium_0">
+										4.
+									</div>
+									<h3 class="elAnim__slide anim__delayMedium_1">
+										We keep shipping and shining. 
+									</h3>
+								</div>
+								<p class="elAnim__slide anim__delayMedium_2">
+									We were selected as one of the most innovative top startups at prestigious tech events like The Web Summit and The Next Web. We keep shipping smarter ways to do voice, all leveraged by our in-house technology.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-carrousel">
+				<div class="bg-1 clr color-1"></div>
+				<div class="wrapper">
+					<div class="bg-2 clr color-1"></div>
+					<div class="obj clr obj-1 color-1">
+						<svg viewBox="0 0 130 133" xmlns="http://www.w3.org/2000/svg">
+							<path d="M42.078-59.673c23.272-9.893 37.163 13.364 45.989 39.484 11.814 34.976 35.158 44.512 39.848 85.517.841 7.245-3.58 67.983-57.427 65.604-30.7-1.342-62.207-40.933-68.025-100.76-3.49-36.317 13.05-78.556 39.615-89.845z" stroke-width="4" fill="none" fill-rule="evenodd"/>
+						</svg>
+					</div>
+					<div class="obj obj-2 clr color-1">
+						<svg viewBox="0 0 149 125" xmlns="http://www.w3.org/2000/svg">
+							<path d="M.619 2.032c19.446 23.836 44.83 49.344 73.212 63.963 63.876 32.9 95.249 65.163 56.765 99.45-38.484 34.287 43.104 76.048-42.868 77.497-49.64.836-135.258-36.758-212.865-87.762" stroke-width="4" fill="none" fill-rule="evenodd"/>
+						</svg>
+					</div>
+					<div class="container-pp">
+						<div class="container-el">
+							<div class="el" style="background-image: url(img/about/section-carrousel/photo-1.png)"></div>
+							<div class="el" style="background-image: url(img/about/section-carrousel/photo-2.png)"></div>
+							<div class="el" style="background-image: url(img/about/section-carrousel/photo-3.png)"></div>
+							<div class="el" style="background-image: url(img/about/section-carrousel/photo-4.png)"></div>
+						</div>
+					</div>
+					<div class="container-text">
+						<div class="container-el">
+							<div class="el">
+								<h5>The team</h5>
+								<h2>Thomas de Becdelievre</h2>
+								<p>CTO of SnapCall</p>
+							</div>
+							<div class="el">
+								<h5>The team</h5>
+								<h2>Benjamin Crot</h2>
+								<p>Sales of SnapCall</p>
+							</div>
+							<div class="el">
+								<h5>The team</h5>
+								<h2>Natalia Diacono</h2>
+								<p>Growth Marketing of SnapCall</p>
+							</div>
+							<div class="el">
+								<h5>The team</h5>
+								<h2>Eve-Anne Collomb</h2>
+								<p>Growth Marketing of SnapCall</p>
+							</div>
+						</div>
+					</div>
+					<div class="container-arrow">
+						<div class="arrow left">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+								<g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="3">
+									<path d="M1.563 10.25h16.875M10.563 18.125l7.874-7.875-7.875-7.875"/>
+								</g>
+							</svg>
+						</div>
+						<div class="arrow right">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+								<g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="3">
+									<path d="M1.563 10.25h16.875M10.563 18.125l7.874-7.875-7.875-7.875"/>
+								</g>
+							</svg>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-partners">
+				<div class="wrapper container-text sectionAnim_container">
+					<h2 class="elAnim__slide anim__delayMedium_0">
+						Invertors<br>and partners.
+					</h2>
+					<div class="container-logos">
+						<div class="logo elAnim__slide anim__delayMedium_1">
+							<img src="img/about/section-partners/logo-isai.svg">
+						</div>
+						<div class="logo elAnim__slide anim__delayMedium_2">
+							<img src="img/about/section-partners/logo-stationF.svg">
+						</div>
+						<div class="logo elAnim__slide anim__delayMedium_3">
+							<img src="img/about/section-partners/logo-zendesk.svg">
+						</div>
+						<div class="logo elAnim__slide anim__delayMedium_4">
+							<img src="img/about/section-partners/logo-lafayette.svg">
+						</div>
+						<div class="logo elAnim__slide anim__delayMedium_5">
+							<img src="img/about/section-partners/logo-franceDigitale.svg">
+						</div>
+						<div class="logo elAnim__slide anim__delayMedium_6">
+							<img src="img/about/section-partners/logo-laFrenchTech.png">
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="cm-section-banner" class="sectionAnim_container">
+				<img class="bg-desktop-medium elAnim__fade anim__delayMedium_0" src="img/common/cm-section-banner-bg-medium.svg" alt="">
+				<img class="bg-desktop-big elAnim__fade anim__delayMedium_0" src="img/common/cm-section-banner-bg-big.svg" alt="">
+
+				<div class="wrapper">
+					<div class="container-text">
+						<h2 class="elAnim__slide anim__delayMedium_1">
+							Sounds like we are building something cool ?
+						</h2>
+						<div class="container-button elAnim__slide anim__delayMedium_2">
+							<a href="" class="cm-button-style1">
+								<span class="btn-text">Get Started</span>
+								<span class="container-btn-arrow">
+									<svg class="btn-arrow" viewBox="0 0 23 22">
+									   <use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/about-min.js"></script>
+
+		<script type="text/javascript" src="script/minify/common-min.js"></script>
+	</body>
+</html>
