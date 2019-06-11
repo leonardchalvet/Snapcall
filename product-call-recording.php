ff91270d386134042a9835f519968bf55bfb25e8
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-06-11 16:18:47 +0200

    video vimeo &quality=540p

--- product-call-recording.php
+++ product-call-recording.php
@@ -88,7 +88,7 @@
 								</p>
 							</div>
 							<div class="container-illu elAnim__fade anim__delayMedium_3">
-								<iframe class="illu" src="https://player.vimeo.com/video/337786561?api=1&loop=1&background=1&mute=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+								<iframe class="illu" src="https://player.vimeo.com/video/337786561?api=1&loop=1&background=1&mute=0&quality=540p" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 							</div>
 						</div>
 					</div>
