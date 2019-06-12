2ed010b48796c34faa7caaf8074b9b67829f6c05
Léonard <leonard@agence-me.com>
2019-06-12 16:33:29 +0200

    Add script + Mailchimp

--- common-footer.php
+++ common-footer.php
@@ -69,15 +69,30 @@
 		</div>
 
 	</div>
-	<div class="container-newletters">
+	<!-- Begin Mailchimp Signup Form -->
+	<div id="mc_embed_signup" class="container-newletters">
 		<h4>Get SnapCall updates. No spam, just fun</h4>
-		<div class="container-input">
-			<input type="text" placeholder="Your email">
-			<svg class="arrow" viewBox="0 0 23 22">
-			   <use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
-			</svg>
-		</div>
+		<form action="https://snapcall.us14.list-manage.com/subscribe/post?u=e1155b6a086a2bd67c7e5ca18&amp;id=0b833a5748" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		    <div id="mc_embed_signup_scroll" class="container-input">
+			    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email" required>
+			    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+			    	<input type="text" name="b_e1155b6a086a2bd67c7e5ca18_0b833a5748" tabindex="-1" value="">
+			    </div>
+			    <div class="clear" id="mc-embedded-subscribe">
+			    	
+			    	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+			    	
+			    	<svg class="arrow" viewBox="0 0 23 22">
+					   <use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+					</svg>
+			    </div>
+		    </div>
+
+		   
+		</form>
 	</div>
+	<!--End mc_embed_signup-->
 	<div class="foot">
 		<div class="wrapper">
 
@@ -148,4 +163,48 @@
 
 		</div>
 	</div>
+
+	
 </footer>
+
+<script type="text/javascript">
+        var _gaq = _gaq || [];_gaq.push(["_setAccount", "UA-96079669-1"]);_gaq.push(["_trackPageview"]);(function() {var ga = document.createElement("script");ga.type = "text/javascript";ga.async = true;ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga, s);})();
+    </script>
+    <script type="text/javascript">
+        (function(o){var b="https://api.autopilothq.com/anywhere/",t="b44a98e81d1f40009617601adc22446dba4b0eb7783746309c14ccd127c44056",a=window.AutopilotAnywhere={_runQueue:[],run:function(){this._runQueue.push(arguments);}},c=encodeURIComponent,s="SCRIPT",d=document,l=d.getElementsByTagName(s)[0],p="t="+c(d.title||"")+"&u="+c(d.location.href||"")+"&r="+c(d.referrer||""),j="text/javascript",z,y;if(!window.Autopilot) window.Autopilot=a;if(o.app) p="devmode=true&"+p;z=function(src,asy){var e=d.createElement(s);e.src=src;e.type=j;e.async=asy;l.parentNode.insertBefore(e,l);};y=function(){z(b+t+'?'+p,true);};if(window.attachEvent){window.attachEvent("onload",y);}else{window.addEventListener("load",y,false);}})({});
+    </script>
+    <script type="text/javascript">
+     (function(a,l,b,c,r,s){
+       _nQc=c,r=a.createElement(l),s=a.getElementsByTagName(l)[0];
+       r.async=1;
+       r.src=l.src=("https:"==a.location.protocol?"https://":"http://")+b;
+       s.parentNode.insertBefore(r,s);})
+       (document,"script","serve.albacross.com/track.js","89220192");
+   </script>
+   <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2284072665141620');
+    fbq('track', 'PageView');
+    </script>
+    <noscript>
+    <img height="1" width="1"
+    src="https://www.facebook.com/tr?id=2284072665141620&ev=PageView
+    &noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+    <!-- Start of snapcall Zendesk Widget script -->
+    <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=d1a134f6-219e-455f-a8d1-349ca32709b4"> </script>
+    <!-- End of snapcall Zendesk Widget script -->
+<!-- Intégration du bouton SnapCall -->
+    <script src="https://snap.snapcall.io/snapapp.min.js" btn-bid="ccdd8a5d9a1711e78f9b0ae03a1ae33f"></script>
+    <!-- Intégration du bouton SnapCall -->
+
+    <script type="text/javascript"> _linkedin_partner_id = "640996"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id); </script><script type="text/javascript"> (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(); </script> <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=640996&fmt=gif" /> </noscript>
